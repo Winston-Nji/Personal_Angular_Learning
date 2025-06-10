5c11ed88235808e88f657c1bbd1086692df7d68d
Winston Nji <winston01russel@gmail.com>
2025-06-10 13:31:36 +0200

    implemented parent to child data-passing

--- angularApp/src/app/app.module.ts
+++ angularApp/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PostComponent } from './post/post.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    PostComponent,
+    PostListComponent,
+    
   ],
   imports: [
     BrowserModule,

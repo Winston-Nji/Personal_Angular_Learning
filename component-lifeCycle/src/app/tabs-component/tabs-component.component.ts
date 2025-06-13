0e10c000dfefbfcd65ee3ee3062e193bee8d8bc8
Winston Nji <winston01russel@gmail.com>
2025-06-13 11:39:21 +0200

    implementation switchCase navigation using [ngswitch], *ngSwitchCase, *ngSwitchDefault

---
+++ component-lifeCycle/src/app/tabs-component/tabs-component.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs-component',
+  templateUrl: './tabs-component.component.html',
+  styleUrl: './tabs-component.component.scss'
+})
+export class TabsComponentComponent {
+
+  tab:string = ''
+
+    navigateToInformation(){
+      this.tab = 'information'
+    }
+
+    navigateToService(){
+    this.tab = "services"
+  }
+
+  navigateToPolicy(){
+    this.tab = 'policy'
+  }
+}
